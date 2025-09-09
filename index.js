c1a16f8f56548b6863e995b28f4a8603a36f799f
multipleof4 <planetrenox@protonmail.com>
2025-09-09 05:19:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// A simple, secure proxy for the Cloudflare API
-export default {
-  async fetch(request) {
-    const corsHeaders = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-    };
-    if (request.method === 'OPTIONS') {
-      return new Response(null, { headers: corsHeaders });
-    }
-    const url = new URL(request.url);
-    const apiUrl = `https://api.cloudflare.com${url.pathname}${url.search}`;
-    const newRequest = new Request(apiUrl, request);
-    const response = await fetch(newRequest);
-    const newResponse = new Response(response.body, response);
-    Object.entries(corsHeaders).forEach(([key, value]) => {
-      newResponse.headers.set(key, value);
-    });
-    return newResponse;
-  },
-};
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+}
+
+async function fetch(request) {
+  if (request.method === 'OPTIONS') return new Response(null, { headers: corsHeaders })
+  return new Response('OK', { headers: corsHeaders })
+}
+
+export default { fetch }
